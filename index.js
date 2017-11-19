c43bd2e02eafda2325e9c8c2324641abeee9586b
Mark Voorberg <mark@binaryops.ca>
2017-11-19 08:44:29 -0800

    Adding Debug to the transom core

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 'use strict';
 
+const debug = require('debug')('transom:core');
 const favicon = require('serve-favicon');
 const path = require('path');
 const restify = require('restify');
@@ -12,6 +13,7 @@ function TransomCore() {
 	const plugins = [];
 
 	this.configure = function (plugin, options) {
+		debug('Adding Transom plugin:', plugin.constructor.name);
 		options = options || {};
 		plugins.push({
 			plugin,
@@ -22,6 +24,7 @@ function TransomCore() {
 	this.initialize = function (server, options) {
 		// Allow users to create their own Server & pass it in along with an options object.
 		if (!options) {
+			debug('Creating new Restify server');
 			options = server || {};
 			server = restify.createServer();
 		}
@@ -33,12 +36,14 @@ function TransomCore() {
 		// Setup a Bunyan logger with request details.
 		const requestLoggerOpts = server.registry.get('transom-options.transom.requestLogger', {});
 		if (requestLoggerOpts) {
+			debug('Adding Restify RequestLogger plugin');
 			server.use(restifyPlugins.requestLogger(requestLoggerOpts));
 		}
 
 		// Use CORS for handling cross-domain ajax requests.
 		const corsOptions = server.registry.get('transom-options.transom.cors', {});
 		if (corsOptions) {
+			debug('Adding CORS handling');
 			// Get an array of valid domain names for CORS and handle OPTIONS requests.
 			corsOptions.origins = corsOptions.origins || ['*'];
 			corsOptions.allowHeaders = (corsOptions.allowHeaders || []).concat(['authorization']);
@@ -51,6 +56,7 @@ function TransomCore() {
 		// Parse body parameters into the req.params object.
 		const bodyOpts = server.registry.get('transom-options.transom.bodyParser', {});
 		if (bodyOpts) {
+			debug('Adding Restify BodyParser plugin');
 			bodyOpts.mapParams = (bodyOpts.mapParams === undefined) ? true : bodyOpts.mapParams; // default true
 			bodyOpts.limit = (bodyOpts.limit === undefined) ? 20000 : bodyOpts.limit; // default 20000
 			server.use(restifyPlugins.bodyParser(bodyOpts));
@@ -59,6 +65,7 @@ function TransomCore() {
 		// Parse query parameters into the req.params object.
 		const queryOpts = server.registry.get('transom-options.transom.queryParser', {});
 		if (queryOpts) {
+			debug('Adding Restify QueryParser plugin');
 			queryOpts.mapParams = (queryOpts.mapParams === undefined) ? true : queryOpts.mapParams; // default true
 			server.use(restifyPlugins.queryParser(queryOpts));
 		}
@@ -66,6 +73,7 @@ function TransomCore() {
 		// Parse url-encoded forms into the req.params object.
 		const encBodyOpts = server.registry.get('transom-options.transom.urlEncodedBodyParser', {});
 		if (encBodyOpts) {
+			debug('Adding Restify UrlEncodedBodyParser plugin');
 			encBodyOpts.mapParams = (encBodyOpts.mapParams === undefined) ? true : encBodyOpts.mapParams; // default true
 			server.use(restifyPlugins.urlEncodedBodyParser(encBodyOpts));
 		}
@@ -73,24 +81,28 @@ function TransomCore() {
 		// Compress API responses with gzip.
 		const gzipOpts = server.registry.get('transom-options.transom.gzipResponse', {});
 		if (gzipOpts) {
+			debug('Adding Restify GzipResponse plugin');
 			server.use(restifyPlugins.gzipResponse(gzipOpts));
 		}
 
 		// Use fullResponse, adding a bunch of Headers to the response.
 		const fullOpts = server.registry.get('transom-options.transom.fullResponse', {});
 		if (fullOpts) {
+			debug('Adding Restify FullResponse plugin');
 			server.use(restifyPlugins.fullResponse(fullOpts));
 		}
 
 		// Provide a transom icon for API GET requests from a browser.
 		const faviconOpts = server.registry.get('transom-options.transom.favicon', {});
 		if (faviconOpts) {
+			debug('Adding Favicon support');
 			faviconOpts.path = faviconOpts.path || path.join(__dirname, 'images', 'favicon.ico');
 			server.use(favicon(faviconOpts.path));
 		}
 
 		// Create req.locals *before* initializing all our plugins!
 		server.use(function (req, res, next) {
+			debug('Initializing req.locals and req.session');
 			req.locals = req.locals || {}; // Required by many of our API methods.
 			req.session = req.session || {}; // required by FacebookStrategy.
 			next();
@@ -99,9 +111,10 @@ function TransomCore() {
 		// Configure each registered plugin, in the order they've been added.
 		for (const each of plugins) {
 			try {
+				debug('Initializing Transom plugin:', each.plugin.constructor.name);
 				each.plugin.initialize(server, each.options);
 			} catch (err) {
-				//console.error("Transom core initialize failed;", err);
+				debug("Transom core initialize failed!", err);
 				throw err;
 			}
 		}
